3050f286ebcd632bf20998d95b967b6a4c2ca435
Sandra Parsick <mail@sandra-parsick.de>
2023-06-21 20:01:46 +0200

    init config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("Hello world")
+  res.render('index', { title: 'Express', mongoUrl: process.env.MONGODB_URI, env: process.env.ENV });
 });
 
 module.exports = router;
